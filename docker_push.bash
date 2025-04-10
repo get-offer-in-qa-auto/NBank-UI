97fd631a7b1ee83f566c02e4b3d145fe2e932585
alex.pshe <alexpshe.help@gmail.com>
2025-04-10 20:09:12 +0400

    Add Dockerfile

---
+++ docker_push.bash
@@ -0,0 +1,9 @@
+DOCKER_USERNAME="nobugsme"
+LOCAL_IMAGE_NAME="nbank'ui"
+LOCAL_IMAGE_TAG="latest"
+REMOTE_IMAGE_TAG="latest"
+
+docker login -u ${DOCKER_USERNAME} -p dckr_pat_CsrFYBFYLZmNK0ODZVz5gppYbtk
+docker tag "${LOCAL_IMAGE_NAME}:${LOCAL_IMAGE_TAG}" "${DOCKER_USERNAME}/nbank-ui:${REMOTE_IMAGE_TAG}"
+docker push "${DOCKER_USERNAME}/nbank-ui:${REMOTE_IMAGE_TAG}"
+
--- restart.bash
+++ restart.bash
@@ -1,5 +1,5 @@
 docker stop nbank-ui
 docker rm nbank-ui
 docker build --no-cache --build-arg REACT_APP_API_VERSION=v1 -t nbank-ui .
-docker run -d -p 3000:80 -e REACT_APP_API_VERSION=v1 --name nbank-ui nbank-ui
+docker run -d -p 3000:80 -e REACT_APP_API_VERSION=v1 --name nbank-ui --network=nbank-network nbank-ui
 
