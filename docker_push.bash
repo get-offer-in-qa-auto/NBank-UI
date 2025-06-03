fdbfdbd0d2004fa015fcee4e289057c41a0c9007
alex.pshe <alexpshe.help@gmail.com>
2025-06-03 19:21:55 -0400

    Update Tag

--- docker_push.bash
+++ docker_push.bash
@@ -3,7 +3,7 @@ LOCAL_IMAGE_NAME="nbank-ui"
 LOCAL_IMAGE_TAG="latest"
 REMOTE_IMAGE_TAG="latest"
 
-docker login -u ${DOCKER_USERNAME} -p dckr_pat_CsrFYBFYLZmNK0ODZVz5gppYbtk
+docker login -u ${DOCKER_USERNAME} -p ${{ secrets.DOCKER_PASSWORD }}
 docker tag "${LOCAL_IMAGE_NAME}:${LOCAL_IMAGE_TAG}" "${DOCKER_USERNAME}/nbank-ui:${REMOTE_IMAGE_TAG}"
 docker push "${DOCKER_USERNAME}/nbank-ui:${REMOTE_IMAGE_TAG}"
 
